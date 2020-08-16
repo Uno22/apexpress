1c01d23141dadf3463b4ee1481c38ed5a8ca9acd
Uno22 <ltung630@gmail.com>
2020-08-16 21:15:22 +0700

    - Update build

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World and Github Action 8:56')
+  res.send('Hello World and Github Action 9:14')
 })
  
 const port = process.env.PORT || 3000;
