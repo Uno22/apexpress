443aafc295d436b56e9563604c58c4f7233b5c3c
Uno22 <ltung630@gmail.com>
2020-08-16 20:43:43 +0700

    - Change text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World and Github Action')
 })
  
 const port = process.env.PORT || 3000;
