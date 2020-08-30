389f0414682e30e52a601f2774a1bc91c4ce94d1
Uno22 <ltung630@gmail.com>
2020-08-30 23:44:41 +0700

    - Update text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World and Github Action and Mocha 9:41')
+  res.send('<h2 style="text-align: center">Welcome to Github Action Demo</h2>')
 })
  
 const port = process.env.PORT || 3000;
